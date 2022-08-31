493c02ef32aef83b74af71e8d100f5f43b8ed58d
Van Vien Dong <vanviendong96@gmail.com>
2022-08-31 23:35:06 +0700

    MVC architecture

---
+++ src/js/model.js
@@ -0,0 +1,28 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+      cookingTime: recipe.cooking_time,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    console.error(`${err} 💥💥💥💥`);
+  }
+};
